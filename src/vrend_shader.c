mittorn/virglrenderer

commit e4880f6bd3d2f02376cabaaacb6937a99c018090

shader: fix UCMP translation

src/vrend_shader.c
@@ -1657,7 +1657,7 @@ iter_instruction(struct tgsi_iterate_context *iter,
       break;
    case TGSI_OPCODE_CMP:
    case TGSI_OPCODE_UCMP:
-      snprintf(buf, 255, "%s = mix(%s, %s, greaterThanEqual(%s, vec4(0.0)))%s;\n", dsts[0], srcs[1], srcs[2], srcs[0], writemask);
+      snprintf(buf, 255, "%s = mix(%s, %s, notEqual(%s, vec4(0.0)))%s;\n", dsts[0], srcs[2], srcs[1], srcs[0], writemask);
       EMIT_BUF_WITH_RET(ctx, buf);
       break;
    case TGSI_OPCODE_END:

commit 7f050c7e5bfd61b6c9de489f049f9a5747fea6d6

shader: restore the original CMP
e4880f6b broke the original CMP translation that looks correct

src/vrend_shader.c
@@ -1714,6 +1714,9 @@ iter_instruction(struct tgsi_iterate_context *iter,
       EMIT_BUF_WITH_RET(ctx, buf);
       break;
    case TGSI_OPCODE_CMP:
+       snprintf(buf, 255, "%s = mix(%s, %s, greaterThanEqual(%s, vec4(0.0)))%s;\n", dsts[0], srcs[1], srcs[2], srcs[0], writemask);
+       EMIT_BUF_WITH_RET(ctx, buf);
+       break;
    case TGSI_OPCODE_UCMP:
       snprintf(buf, 255, "%s = mix(%s, %s, notEqual(%s, vec4(0.0)))%s;\n", dsts[0], srcs[2], srcs[1], srcs[0], writemask);
       EMIT_BUF_WITH_RET(ctx, buf);