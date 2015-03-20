0a55b3264cee34111b3898ba4597c4b5c451bf80
Dave Airlie <airlied@redhat.com>
2015-03-20 13:12:12 +1000

    renderer: split out draw time sampler/ubo setup to separate functions
    Trying to clean up the draw vbo functions a little.
    
    The commit looks wierd, the final code looks better

--- src/vrend_renderer.c
+++ src/vrend_renderer.c
@@ -2390,13 +2390,116 @@ static void vrend_draw_bind_vertex_binding(struct vrend_context *ctx,
    }
 }
 
+static void vrend_draw_bind_samplers(struct vrend_context *ctx)
+{
+   int sampler_id;
+   int i;
+   int shader_type;
+
+   sampler_id = 0;
+   for (shader_type = PIPE_SHADER_VERTEX; shader_type <= (ctx->sub->gs ? PIPE_SHADER_GEOMETRY : PIPE_SHADER_FRAGMENT); shader_type++) {
+      int index = 0;
+      for (i = 0; i < ctx->sub->views[shader_type].num_views; i++) {
+         struct vrend_resource *texture = NULL;
+
+         if (ctx->sub->views[shader_type].views[i]) {
+            texture = ctx->sub->views[shader_type].views[i]->texture;
+         }
+
+         if (!(ctx->sub->prog->samplers_used_mask[shader_type] & (1 << i)))
+            continue;
+
+         if (ctx->sub->prog->samp_locs[shader_type])
+            glUniform1i(ctx->sub->prog->samp_locs[shader_type][index], sampler_id);
+
+         if (ctx->sub->prog->shadow_samp_mask[shader_type] & (1 << i)) {
+            struct vrend_sampler_view *tview = ctx->sub->views[shader_type].views[i];
+            glUniform4f(ctx->sub->prog->shadow_samp_mask_locs[shader_type][index], 
+                        tview->gl_swizzle_r == GL_ZERO ? 0.0 : 1.0, 
+                        tview->gl_swizzle_g == GL_ZERO ? 0.0 : 1.0, 
+                        tview->gl_swizzle_b == GL_ZERO ? 0.0 : 1.0, 
+                        tview->gl_swizzle_a == GL_ZERO ? 0.0 : 1.0);
+            glUniform4f(ctx->sub->prog->shadow_samp_add_locs[shader_type][index], 
+                        tview->gl_swizzle_r == GL_ONE ? 1.0 : 0.0, 
+                        tview->gl_swizzle_g == GL_ONE ? 1.0 : 0.0, 
+                        tview->gl_swizzle_b == GL_ONE ? 1.0 : 0.0, 
+                        tview->gl_swizzle_a == GL_ONE ? 1.0 : 0.0);
+         }
+            
+         glActiveTexture(GL_TEXTURE0 + sampler_id);
+         if (texture) {
+            int id;
+
+            if (texture->target == GL_TEXTURE_BUFFER)
+               id = texture->tbo_tex_id;
+            else
+               id = texture->id;
+
+            glBindTexture(texture->target, id);
+            if (ctx->sub->views[shader_type].old_ids[i] != id || ctx->sub->sampler_state_dirty) {
+               vrend_apply_sampler_state(ctx, texture, shader_type, i, sampler_id, ctx->sub->views[shader_type].views[i]->srgb_decode);
+               ctx->sub->views[shader_type].old_ids[i] = id;
+            }
+            if (ctx->sub->rs_state.point_quad_rasterization) {
+               if (use_core_profile == 0) {
+                  if (ctx->sub->rs_state.sprite_coord_enable & (1 << i))
+                     glTexEnvi(GL_POINT_SPRITE_ARB, GL_COORD_REPLACE_ARB, GL_TRUE);
+                  else
+                     glTexEnvi(GL_POINT_SPRITE_ARB, GL_COORD_REPLACE_ARB, GL_FALSE);
+               }
+            }
+            sampler_id++;
+         }
+         index++;
+      }
+   }
+
+   if (use_core_profile && ctx->sub->prog->fs_stipple_loc != -1) {
+      glActiveTexture(GL_TEXTURE0 + sampler_id);
+      glBindTexture(GL_TEXTURE_2D, ctx->pstipple_tex_id);
+      glUniform1i(ctx->sub->prog->fs_stipple_loc, sampler_id);
+   }
+   ctx->sub->sampler_state_dirty = FALSE;
+}
+
+static void vrend_draw_bind_ubo(struct vrend_context *ctx)
+{
+   int i;
+   int ubo_id;
+   int shader_type;
+
+   ubo_id = 0;
+   for (shader_type = PIPE_SHADER_VERTEX; shader_type <= (ctx->sub->gs ? PIPE_SHADER_GEOMETRY : PIPE_SHADER_FRAGMENT); shader_type++) {
+      uint32_t mask;
+      int shader_ubo_idx = 0;
+      struct pipe_constant_buffer *cb;
+      struct vrend_resource *res;
+      if (!ctx->sub->const_bufs_used_mask[shader_type])
+         continue;
+
+      if (!ctx->sub->prog->ubo_locs[shader_type])
+         continue;
+
+      mask = ctx->sub->const_bufs_used_mask[shader_type];
+      while (mask) {
+         i = u_bit_scan(&mask);
+
+         cb = &ctx->sub->cbs[shader_type][i];
+         res = (struct vrend_resource *)cb->buffer;
+         glBindBufferRange(GL_UNIFORM_BUFFER, ubo_id, res->id,
+                           cb->buffer_offset, cb->buffer_size);
+         glUniformBlockBinding(ctx->sub->prog->id, ctx->sub->prog->ubo_locs[shader_type][shader_ubo_idx], ubo_id);
+         shader_ubo_idx++;
+         ubo_id++;
+      }
+   }
+}
+
 void vrend_draw_vbo(struct vrend_context *ctx,
                     const struct pipe_draw_info *info,
                     uint32_t cso)
 {
    int i;
-   int sampler_id;
-   int ubo_id;
    bool new_program = FALSE;
    uint32_t shader_type;
 
@@ -2448,7 +2551,7 @@ void vrend_draw_vbo(struct vrend_context *ctx,
    }
 
    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, ctx->sub->fb_id);
-   
+
    vrend_use_program(ctx, ctx->sub->prog->id);
 
    for (shader_type = PIPE_SHADER_VERTEX; shader_type <= (ctx->sub->gs ? PIPE_SHADER_GEOMETRY : PIPE_SHADER_FRAGMENT); shader_type++) {
@@ -2469,91 +2572,9 @@ void vrend_draw_vbo(struct vrend_context *ctx,
       }
    }
 
-   sampler_id = 0;
-   for (shader_type = PIPE_SHADER_VERTEX; shader_type <= (ctx->sub->gs ? PIPE_SHADER_GEOMETRY : PIPE_SHADER_FRAGMENT); shader_type++) {
-      int index = 0;
-      for (i = 0; i < ctx->sub->views[shader_type].num_views; i++) {
-         struct vrend_resource *texture = NULL;
+   vrend_draw_bind_samplers(ctx);
 
-         if (ctx->sub->views[shader_type].views[i]) {
-            texture = ctx->sub->views[shader_type].views[i]->texture;
-         }
-
-         if (!(ctx->sub->prog->samplers_used_mask[shader_type] & (1 << i)))
-             continue;
-
-         if (ctx->sub->prog->samp_locs[shader_type])
-            glUniform1i(ctx->sub->prog->samp_locs[shader_type][index], sampler_id);
-
-         if (ctx->sub->prog->shadow_samp_mask[shader_type] & (1 << i)) {
-            struct vrend_sampler_view *tview = ctx->sub->views[shader_type].views[i];
-            glUniform4f(ctx->sub->prog->shadow_samp_mask_locs[shader_type][index], 
-                        tview->gl_swizzle_r == GL_ZERO ? 0.0 : 1.0, 
-                        tview->gl_swizzle_g == GL_ZERO ? 0.0 : 1.0, 
-                        tview->gl_swizzle_b == GL_ZERO ? 0.0 : 1.0, 
-                        tview->gl_swizzle_a == GL_ZERO ? 0.0 : 1.0);
-            glUniform4f(ctx->sub->prog->shadow_samp_add_locs[shader_type][index], 
-                        tview->gl_swizzle_r == GL_ONE ? 1.0 : 0.0, 
-                        tview->gl_swizzle_g == GL_ONE ? 1.0 : 0.0, 
-                        tview->gl_swizzle_b == GL_ONE ? 1.0 : 0.0, 
-                        tview->gl_swizzle_a == GL_ONE ? 1.0 : 0.0);
-         }
-            
-         glActiveTexture(GL_TEXTURE0 + sampler_id);
-         if (texture) {
-            int id;
-
-            if (texture->target == GL_TEXTURE_BUFFER)
-               id = texture->tbo_tex_id;
-            else
-               id = texture->id;
-
-            glBindTexture(texture->target, id);
-            if (ctx->sub->views[shader_type].old_ids[i] != id || ctx->sub->sampler_state_dirty) {
-               vrend_apply_sampler_state(ctx, texture, shader_type, i, sampler_id, ctx->sub->views[shader_type].views[i]->srgb_decode);
-               ctx->sub->views[shader_type].old_ids[i] = id;
-            }
-            if (ctx->sub->rs_state.point_quad_rasterization) {
-               if (use_core_profile == 0) {
-                  if (ctx->sub->rs_state.sprite_coord_enable & (1 << i))
-                     glTexEnvi(GL_POINT_SPRITE_ARB, GL_COORD_REPLACE_ARB, GL_TRUE);
-                  else
-                     glTexEnvi(GL_POINT_SPRITE_ARB, GL_COORD_REPLACE_ARB, GL_FALSE);
-               }
-            }
-            sampler_id++;
-         }
-         index++;
-      }
-   } 
-
-   ubo_id = 0;
-   for (shader_type = PIPE_SHADER_VERTEX; shader_type <= (ctx->sub->gs ? PIPE_SHADER_GEOMETRY : PIPE_SHADER_FRAGMENT); shader_type++) {
-      uint32_t mask;
-      int shader_ubo_idx = 0;
-      struct pipe_constant_buffer *cb;
-      struct vrend_resource *res;
-      if (!ctx->sub->const_bufs_used_mask[shader_type])
-         continue;
-
-      if (!ctx->sub->prog->ubo_locs[shader_type])
-         continue;
-
-      mask = ctx->sub->const_bufs_used_mask[shader_type];
-      while (mask) {
-         i = u_bit_scan(&mask);
-
-         cb = &ctx->sub->cbs[shader_type][i];
-         res = (struct vrend_resource *)cb->buffer;
-         glBindBufferRange(GL_UNIFORM_BUFFER, ubo_id, res->id,
-                           cb->buffer_offset, cb->buffer_size);
-         glUniformBlockBinding(ctx->sub->prog->id, ctx->sub->prog->ubo_locs[shader_type][shader_ubo_idx], ubo_id);
-         shader_ubo_idx++;
-         ubo_id++;
-      }
-   }
-
-   ctx->sub->sampler_state_dirty = FALSE;
+   vrend_draw_bind_ubo(ctx);
 
    if (!ctx->sub->ve) {
       fprintf(stderr,"illegal VE setup - skipping renderering\n");
@@ -2561,11 +2582,6 @@ void vrend_draw_vbo(struct vrend_context *ctx,
    }
    glUniform4f(ctx->sub->prog->vs_ws_adjust_loc, 0.0, ctx->sub->viewport_is_negative ? -1.0 : 1.0, ctx->sub->depth_scale, ctx->sub->depth_transform);
 
-   if (use_core_profile && ctx->sub->prog->fs_stipple_loc != -1) {
-      glActiveTexture(GL_TEXTURE0 + sampler_id);
-      glBindTexture(GL_TEXTURE_2D, ctx->pstipple_tex_id);
-      glUniform1i(ctx->sub->prog->fs_stipple_loc, sampler_id);
-   }
 
    if (ctx->sub->rs_state.clip_plane_enable) {
       for (i = 0 ; i < 8; i++) {
