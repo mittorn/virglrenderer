36492a4012f127c4d8d789f57d3959a2a65caa56
Marc-André Lureau <marcandre.lureau@redhat.com>
2016-01-19 14:44:24 +0100

    decode: validate shader type
    Fix found thanks to american fuzzy lop.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- src/vrend_decode.c
+++ src/vrend_decode.c
@@ -219,6 +219,10 @@ static int vrend_decode_set_constant_buffer(struct vrend_decode_ctx *ctx, uint16
 
    shader = get_buf_entry(ctx, VIRGL_SET_CONSTANT_BUFFER_SHADER_TYPE);
    index = get_buf_entry(ctx, VIRGL_SET_CONSTANT_BUFFER_INDEX);
+
+   if (shader >= PIPE_SHADER_TYPES)
+      return EINVAL;
+
    vrend_set_constants(ctx->grctx, shader, index, nc, get_buf_ptr(ctx, VIRGL_SET_CONSTANT_BUFFER_DATA_START));
    return 0;
 }
