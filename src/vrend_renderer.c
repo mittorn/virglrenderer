221006de6c9e124806a92f2299bdffb36cbd1e7c
Marc-André Lureau <marcandre.lureau@gmail.com>
2016-01-21 17:12:44 +0100

    vrend: check eventfd value before close
    Signed-off-by: Marc-André Lureau <marcandre.lureau@gmail.com>

--- src/vrend_renderer.c
+++ src/vrend_renderer.c
@@ -3731,8 +3731,10 @@ vrend_renderer_fini(void)
       return;
 
    vrend_free_sync_thread();
-   close(vrend_state.eventfd);
-   vrend_state.eventfd = -1;
+   if (vrend_state.eventfd == -1) {
+      close(vrend_state.eventfd);
+      vrend_state.eventfd = -1;
+   }
 
    vrend_decode_reset(false);
    vrend_object_fini_resource_table();
