40b0e7813325b08077b6f541b3989edb2d86d837
Li Qiang <liq3ea@gmail.com>
2016-12-26 21:23:42 -0500

    renderer: fix a leak in resource attach
    Just return if the resource has been attached a iov
    to avoid memory leak.
    
    Signed-off-by: Li Qiang <liq3ea@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/vrend_renderer.c
+++ src/vrend_renderer.c
@@ -4031,6 +4031,9 @@ int vrend_renderer_resource_attach_iov(int res_handle, struct iovec *iov,
    if (!res)
       return EINVAL;
 
+   if (res->iov)
+      return 0;
+
    /* work out size and max resource size */
    res->iov = iov;
    res->num_iovs = num_iovs;
