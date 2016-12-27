747a293ff6055203e529f083896b823e22523fe7
Li Qiang <liq3ea@gmail.com>
2016-12-26 21:23:43 -0500

    vrend: fix a leak in context create internal
    Create a context more than once causes memory leak issue.
    Juest return if the context exists.
    
    Signed-off-by: Li Qiang <liq3ea@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/vrend_decode.c
+++ src/vrend_decode.c
@@ -1045,6 +1045,10 @@ void vrend_renderer_context_create_internal(uint32_t handle, uint32_t nlen,
    if (handle >= VREND_MAX_CTX)
       return;
 
+   dctx = dec_ctx[handle];
+   if (dctx)
+      return;
+
    dctx = malloc(sizeof(struct vrend_decode_ctx));
    if (!dctx)
       return;
