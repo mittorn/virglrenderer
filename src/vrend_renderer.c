ba65735fb6bdfe4a8d5e3878a4ffb0abb2ec0727
Jakob Bornecrantz <jakob.bornecrantz@collabora.com>
2017-12-22 18:57:43 +0000

    vrend: Use more available glGetQueryObjectuiv
    The glGetQueryObjectuiv is available both in GLES and in Desktop OpenGL.
    While glGetQueryObjectiv is not available in GLES.
    
    Signed-off-by: Jakob Bornecrantz <jakob.bornecrantz@collabora.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/vrend_renderer.c
+++ src/vrend_renderer.c
@@ -5756,11 +5756,11 @@ void vrend_renderer_check_fences(void)
 
 static bool vrend_get_one_query_result(GLuint query_id, bool use_64, uint64_t *result)
 {
-   GLint ready;
+   GLuint ready;
    GLuint passed;
    GLuint64 pass64;
 
-   glGetQueryObjectiv(query_id, GL_QUERY_RESULT_AVAILABLE_ARB, &ready);
+   glGetQueryObjectuiv(query_id, GL_QUERY_RESULT_AVAILABLE_ARB, &ready);
 
    if (!ready)
       return false;
