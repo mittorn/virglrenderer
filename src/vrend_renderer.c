mittorn/virglrenderer

commit bab29c2243fcfd78211ed7356bbd7b6c0f2cae5d

vrend_clear: clear and restore rasterizer discard and depth clamp
vrend_clear is passed in from gallium clear which, unlike glClear, must
not obey rasterizer discard and depth_clmap. However, the guest doesn't
foreard the latest update of the rendering states to the host before issuing
"clear". Therefore, the host clear command must clear these states before
calling glClear in turn. After calling glClear these states are again brought
into sync with what the host assumes.

Fixes CTS:
   dEQP-GLES3.functional.rasterizer_discard.fbo.*

Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Signed-off-by: Jakob Bornecrantz <jakob@collabora.com>

src/vrend_renderer.c
@@ -2485,12 +2485,17 @@ void vrend_clear(struct vrend_context *ctx,
       } else {
          glClearDepth(depth);
       }
+      if (!ctx->sub->hw_rs_state.depth_clip)
+          glDisable(GL_DEPTH_CLAMP);
    }
 
    if (buffers & PIPE_CLEAR_STENCIL) {
       glStencilMask(~0u);
       glClearStencil(stencil);
    }
+
+   if (ctx->sub->hw_rs_state.rasterizer_discard)
+       glDisable(GL_RASTERIZER_DISCARD);
 
    if (buffers & PIPE_CLEAR_COLOR) {
       uint32_t mask = 0;
@@ -2525,9 +2530,19 @@ void vrend_clear(struct vrend_context *ctx,
    if (bits)
       glClear(bits);
 
-   if (buffers & PIPE_CLEAR_DEPTH)
+   /* Is it really necessary to restore the old states? The only reason we
+    * get here is because the guest cleared all those states but gallium
+    * didn't forward them before calling the clear command
+    */
+   if (ctx->sub->hw_rs_state.rasterizer_discard)
+       glEnable(GL_RASTERIZER_DISCARD);
+
+   if (buffers & PIPE_CLEAR_DEPTH) {
       if (!ctx->sub->dsa_state.depth.writemask)
          glDepthMask(GL_FALSE);
+      if (!ctx->sub->hw_rs_state.depth_clip)
+          glEnable(GL_DEPTH_CLAMP);
+   }
 
    /* Restore previous stencil buffer write masks for both front and back faces */
    if (buffers & PIPE_CLEAR_STENCIL) {

commit 04884aa7e47942cc614a1ce8cac2f59e524fc307

Don't handle rasterizer state depth-clip in virgl_clear
Fixes: bab29c2243fcfd78211ed7356bbd7b6c0f2cae5d
 vrend_clear: clear and restore rasterizer discard and depth clamp

Disabling GL_DEPTH_CLAMP is not needed for the rasterizer_discard tests
and it actually breaks

 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.2
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.10
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.11
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.14
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.16
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.25
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.26
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.39
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.40
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.41
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.56
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.61
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.68
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.70
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.73
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.90
 dEQP-GLES3.functional.fragment_ops.interaction.basic_shader.96

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Signed-off-by: Jakob Bornecrantz <jakob@collabora.com>
Reviewed-by: Jakob Bornecrantz <jakob@collabora.com>
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>

src/vrend_renderer.c
@@ -2486,8 +2486,6 @@ void vrend_clear(struct vrend_context *ctx,
       } else {
          glClearDepth(depth);
       }
-      if (!ctx->sub->hw_rs_state.depth_clip)
-          glDisable(GL_DEPTH_CLAMP);
    }
 
    if (buffers & PIPE_CLEAR_STENCIL) {
@@ -2541,8 +2539,6 @@ void vrend_clear(struct vrend_context *ctx,
    if (buffers & PIPE_CLEAR_DEPTH) {
       if (!ctx->sub->dsa_state.depth.writemask)
          glDepthMask(GL_FALSE);
-      if (!ctx->sub->hw_rs_state.depth_clip)
-          glEnable(GL_DEPTH_CLAMP);
    }
 
    /* Restore previous stencil buffer write masks for both front and back faces */