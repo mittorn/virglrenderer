dfe25353495990f8f13fbc4868b0d6ae50076b1c
Gert Wollny <gert.wollny@collabora.com>
2018-05-30 15:25:57 +0200

    gallium/aux/util/u_debug_refcnt.h: Fix warnings:
    u_debug_refcnt.h: In function 'debug_reference':
    
    u_debug_refcnt.h:39:65: warning: unused parameter 'p'
    u_debug_refcnt.h:39:95: warning: unused parameter 'get_desc'
    u_debug_refcnt.h:39:109: warning: unused parameter 'change'
    
    [-Wunused-parameter]
     static inline void debug_reference(const struct pipe_reference* p,
    debug_reference_descriptor get_desc, int change)
    
    v2: eliminate use of VREND_UNUSED (Erik)
    
    Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
    Signed-off-by: Jakob Bornecrantz <jakob@collabora.com>

--- src/gallium/auxiliary/util/u_debug_refcnt.h
+++ src/gallium/auxiliary/util/u_debug_refcnt.h
@@ -30,13 +30,17 @@
 #include "pipe/p_config.h"
 #include "pipe/p_state.h"
 
+#include "util/u_debug.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef void (*debug_reference_descriptor)(char*, const struct pipe_reference*);
 
-static inline void debug_reference(const struct pipe_reference* p, debug_reference_descriptor get_desc, int change)
+static inline void
+debug_reference(UNUSED const struct pipe_reference* p,
+                UNUSED debug_reference_descriptor get_desc, UNUSED int change)
 {
 }
 
