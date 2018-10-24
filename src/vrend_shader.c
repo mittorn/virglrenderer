mittorn/virglrenderer

commit 36c919e139f4acf89f81fed759b57869c069196a

shader: rework precise-emitting for built-ins
In 4732489 (shader: do not redeclare built-ins as precise), I confused a
mesa shader-compiler bug with a spec problem. Turns out, redeclaring
built-ins as precise is allowed by the spec, just the same as invariant
variables. And even mesa allows this.

What mesa doesn't currently allow, is to redeclare a variable as *both*
invariant *and* precise. This problem does not extend to new
declarations, where this is already handled correctly.

So, let's avoid emitting both; it's not really needed to emit both, as
they essentially have the same effect (disable reordering), just with
some different usage-semantics. And for the cases we support, these are
effectively the same.

This fixes dEQP-GLES31.functional.tessellation.common_edge.*_precise
without breaking dEQP-GLES2.functional.shaders.algorithm.hsl_to_rgb_vertex
this time.

Signed-off-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
Signed-off-by: Dave Airlie <airlied@redhat.com>

src/vrend_shader.c
@@ -4577,9 +4577,10 @@ static char *emit_ios(struct dump_ctx *ctx, char *glsl_hdr)
                         ctx->outputs[i].fbfetch_used ? "inout" : "out",
                         ctx->outputs[i].glsl_name);
             STRCAT_WITH_RET(glsl_hdr, buf);
-         } else if (ctx->outputs[i].invariant) {
-            snprintf(buf, 255, "%s %s;\n",
-               ctx->outputs[i].invariant ? "invariant " : "",
+         } else if (ctx->outputs[i].invariant || ctx->outputs[i].precise) {
+            snprintf(buf, 255, "%s%s;\n",
+               ctx->outputs[i].precise ? "precise " :
+               (ctx->outputs[i].invariant ? "invariant " : ""),
                ctx->outputs[i].glsl_name);
             STRCAT_WITH_RET(glsl_hdr, buf);
          }

commit 860347164c964ea7edc520e88fe3113ef9f6f092

shader: Don't emit 'precise' when 'invariant' is already given in declaration
It is not necessary to emit both statements, and on a GLES 3.1 host that doesn't
support ARB_gpu_shader5 this actually breaks some shaders.

Fixes: 36c919e139f4a
   shader: rework precise-emitting for built-ins

Fixes on the softpipe host driver:

  dEQP-GLES3.functional.shaders.qualification_order.variables.
     valid.invariant_interp_storage
     valid.invariant_interp_storage_precision
     valid.invariant_storage
     valid.invariant_storage_precision

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Signed-off-by: Jakob Bornecrantz <jakob@collabora.com>

src/vrend_shader.c
@@ -2784,8 +2784,10 @@ get_destination_info(struct dump_ctx *ctx,
             if (ctx->outputs[j].first == dst_reg->Register.Index) {
 
                if (inst->Instruction.Precise) {
-                  ctx->outputs[j].precise = true;
-                  ctx->shader_req_bits |= SHADER_REQ_GPU_SHADER5;
+                  if (!ctx->outputs[j].invariant) {
+                     ctx->outputs[j].precise = true;
+                     ctx->shader_req_bits |= SHADER_REQ_GPU_SHADER5;
+                  }
                }
 
                if (ctx->glsl_ver_required >= 140 && ctx->outputs[j].name == TGSI_SEMANTIC_CLIPVERTEX) {