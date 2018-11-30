mittorn/virglrenderer

commit 70dedae46586ad148dec956a1670a0df2f28317a

shader: TXQ: also emit a write mask if only one bit is set.
v2: remove biotcount check since it is always >0 (Dave)

Closes: #58
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
Signed-off-by: Dave Airlie <airlied@redhat.com>

src/vrend_shader.c
@@ -1896,7 +1896,7 @@ static int emit_txq(struct dump_ctx *ctx,
    }
 
    if (inst->Dst[0].Register.WriteMask & 0x7) {
-      snprintf(buf, 255, "%s%s = %s(textureSize(%s%s))%s;\n", dsts[0], get_wm_string(twm), get_string(dtypeprefix), srcs[sampler_index], bias, util_bitcount(inst->Dst[0].Register.WriteMask) > 1 ? writemask : "");
+      snprintf(buf, 255, "%s%s = %s(textureSize(%s%s))%s;\n", dsts[0], get_wm_string(twm), get_string(dtypeprefix), srcs[sampler_index], bias, writemask);
       EMIT_BUF_WITH_RET(ctx, buf);
    }
    return 0;

commit aa302e4b24efd2974b37bb9a584f7dc3b25f649a

shader: TXQ: Don't emit swizzle if we query the size of a buffer
The buffer size is not an vector, and consequently we can't add a swizzle
operator. (This was probably the reason why the write mask was only emmited
when the it was larger then 1 before).

v2: Also handle 1D and Shadow1D textures (Dave)

Fixes: 70dedae46586ad148dec956a1670a0df2f28317a
  shader: TXQ: also emit a write mask if only one bit is set.

Tested-by: Jakob Bornecrantz <jakob@collabora.com>
Reviewed-by: Jakob Bornecrantz <jakob@collabora.com>
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Signed-off-by: Jakob Bornecrantz <jakob@collabora.com>

src/vrend_shader.c
@@ -1899,7 +1899,11 @@ static int emit_txq(struct dump_ctx *ctx,
    }
 
    if (inst->Dst[0].Register.WriteMask & 0x7) {
-      snprintf(buf, 255, "%s%s = %s(textureSize(%s%s))%s;\n", dsts[0], get_wm_string(twm), get_string(dtypeprefix), srcs[sampler_index], bias, writemask);
+      bool txq_returns_vec = (inst->Texture.Texture != TGSI_TEXTURE_BUFFER &&
+                              inst->Texture.Texture != TGSI_TEXTURE_1D &&
+                              inst->Texture.Texture != TGSI_TEXTURE_SHADOW1D);
+      snprintf(buf, 255, "%s%s = %s(textureSize(%s%s))%s;\n", dsts[0], get_wm_string(twm), get_string(dtypeprefix), srcs[sampler_index], bias,
+            txq_returns_vec ? writemask : "");
       EMIT_BUF_WITH_RET(ctx, buf);
    }
    return 0;