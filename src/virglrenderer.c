4aa32df2cf92df037ec633553a52f2c21cab06a1
Dave Airlie <airlied@redhat.com>
2019-01-08 17:20:20 +1000

    fix crash with qemu / egl colorspace
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/virglrenderer.c
+++ src/virglrenderer.c
@@ -205,7 +205,8 @@ int virgl_has_gl_colorspace(void)
 {
    bool egl_colorspace = false;
 #ifdef HAVE_EPOXY_EGL_H
-   egl_colorspace = virgl_has_egl_khr_gl_colorspace(egl_info);
+   if (egl_info)
+      egl_colorspace = virgl_has_egl_khr_gl_colorspace(egl_info);
 #endif
    return use_context == CONTEXT_GLX ||
          (use_context == CONTEXT_EGL && egl_colorspace);
