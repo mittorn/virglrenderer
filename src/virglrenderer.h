mittorn/virglrenderer

commit 70fefe33df3cd39239f16cd626094c391c9a82b4

virgl: expose function to set output callback
Since this adds a new function to the library interface this also needs
a bump in the library version number.

Closes: #80

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: David Riley <davidriley@chromium.org>
Reviewed-by: Zach Reizner <zachr@chromium.org>
Signed-off-by: Dave Airlie <airlied@redhat.com>

src/virglrenderer.h
@@ -27,8 +27,11 @@
 #ifndef VIRGLRENDERER_H
 #define VIRGLRENDERER_H
 
+#include "vrend_debug.h"
+
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdarg.h>
 
 struct virgl_box;
 struct iovec;
@@ -158,6 +161,8 @@ VIRGL_EXPORT void virgl_renderer_force_ctx_0(void);
 
 VIRGL_EXPORT void virgl_renderer_ctx_attach_resource(int ctx_id, int res_handle);
 VIRGL_EXPORT void virgl_renderer_ctx_detach_resource(int ctx_id, int res_handle);
+
+VIRGL_EXPORT virgl_debug_callback_type virgl_set_debug_callback(virgl_debug_callback_type cb);
 
 /* return information about a resource */
 

src/Makefile.am
@@ -51,7 +51,7 @@ endif
 lib_LTLIBRARIES = libvirglrenderer.la
 noinst_LTLIBRARIES = libvrend.la
 
-GM_LDFLAGS = -Wl,-Bsymbolic -version-number 0:2 -no-undefined
+GM_LDFLAGS = -Wl,-Bsymbolic -version-number 0:3 -no-undefined
 
 libvirglrenderer_la_SOURCES = virglrenderer.c
 libvirglrenderer_ladir = $(libdir)

src/virglrenderer.c
@@ -391,3 +391,8 @@ int virgl_renderer_get_poll_fd(void)
 {
    return vrend_renderer_get_poll_fd();
 }
+
+virgl_debug_callback_type virgl_set_debug_callback(virgl_debug_callback_type cb)
+{
+   return vrend_set_debug_callback(cb);
+}

commit 70ae2c3894cff1954d98f6c384170483d23aeb2d

vrenderer: avoid haing to include vrend_debug.h in virglerenderer.h
vrend_debug.h is not installed but we need the typedef for the debug
callback.

Fixes: 70fefe33df3cd39239f16cd626094c391c9a82b4
  virgl: expose function to set output callback

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Signed-off-by: Dave Airlie <airlied@redhat.com>

src/virglrenderer.h
@@ -27,8 +27,6 @@
 #ifndef VIRGLRENDERER_H
 #define VIRGLRENDERER_H
 
-#include "vrend_debug.h"
-
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdarg.h>
@@ -113,6 +111,8 @@ struct virgl_renderer_resource_create_args {
 };
 
 /* new API */
+/* This typedef must be kept in sync with vrend_debug.h */
+typedef void (*virgl_debug_callback_type)(const char *fmt, va_list ap);
 
 VIRGL_EXPORT int virgl_renderer_resource_create(struct virgl_renderer_resource_create_args *args, struct iovec *iov, uint32_t num_iovs);
 VIRGL_EXPORT int virgl_renderer_resource_import_eglimage(struct virgl_renderer_resource_create_args *args, void *image);