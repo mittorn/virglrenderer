mittorn/virglrenderer

commit d9a4be4d0a5b2239734638750c23233c7146fb7e

renderer: remove dead code
tgsitargettogltarget never returns a value that evaluates to zero, so
this condition can't trigger. Let's drop the code.

Signed-off-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
Signed-off-by: Dave Airlie <airlied@redhat.com>

src/vrend_renderer.c
@@ -1802,8 +1802,6 @@ int vrend_create_sampler_view(struct vrend_context *ctx,
    vrend_resource_reference(&view->texture, res);
 
    view->id = view->texture->id;
-   if (!view->target)
-      view->target = view->texture->target;
 
    if (has_feature(feat_texture_view) && !view->texture->is_buffer)  {
       enum pipe_format format;

commit 8d711a1ec2457525b9eb1be2a13db6a483dc075b

vrend: effectively revert "renderer: remove dead code"
d9a4be4d0a5b2239734638750c23233c7146fb7e broke a number of tests
when running on a GLES host because tgsitargettogltarget may
return PIPE_BUFFER which is 0. So, instead of simply reverting the commit
make it more explicit that we check for PIPE_BUFFER to make it clearer
that this condition may actually trigger.

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Signed-off-by: Jakob Bornecrantz <jakob@collabora.com>

src/vrend_renderer.c
@@ -1812,6 +1812,8 @@ int vrend_create_sampler_view(struct vrend_context *ctx,
    vrend_resource_reference(&view->texture, res);
 
    view->id = view->texture->id;
+   if (view->target == PIPE_BUFFER)
+      view->target = view->texture->target;
 
    view->srgb_decode = GL_DECODE_EXT;
    if (view->format != view->texture->base.format) {