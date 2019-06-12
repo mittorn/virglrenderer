9189e19676f63e6bd3c571a901c3a79f56423e3b
David Riley <davidriley@chromium.org>
2019-06-12 14:13:13 -0700

    virglrenderer: Add VIRGL_RES_BIND_SHARED bind flag.
    Allow external users to intercept resource creation based on
    shared bind flag.
    
    Signed-off-by: David Riley <davidriley@chromium.org>
    Reviewed-By: Gert Wollny <gert.wollny@collabora.com>

--- src/virglrenderer.h
+++ src/virglrenderer.h
@@ -96,6 +96,7 @@ VIRGL_EXPORT int virgl_renderer_get_fd_for_texture2(uint32_t tex_id, int *fd, in
 #define VIRGL_RES_BIND_CURSOR        (1 << 16)
 #define VIRGL_RES_BIND_CUSTOM        (1 << 17)
 #define VIRGL_RES_BIND_SCANOUT       (1 << 18)
+#define VIRGL_RES_BIND_SHARED        (1 << 20)
 
 struct virgl_renderer_resource_create_args {
    uint32_t handle;
