mittorn/virglrenderer

commit c20a5a918afd7abfb367672b27987f4769f17ce8

ci: move outdated results to an archived folder amdgpu, llvmpipe and i915 are not used by the freedesktop ci and didn't see any update for at least 6 months.
Signed-off-by: Elie Tournier <elie.tournier@collabora.com>
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>

ci/run_test_suite.sh
@@ -297,7 +297,14 @@ create_result_dir()
 
    TEST_PATH=${HOST_GL}_host${HOST_DRIVER}/${TEST_APP}_${TEST_NAME}
    RESULTS_DIR=$VIRGL_PATH/results/${TEST_PATH}
-   PREVIOUS_RESULTS_DIR=$VIRGL_PATH/ci/previous_results/${TEST_PATH}
+
+   if [ $HOST_DRIVER = softpipe ]; then
+      PREVIOUS_RESULTS_DIR=$VIRGL_PATH/ci/previous_results/${TEST_PATH}
+   else
+      echo WARNING: Results are not up-to-date!
+      PREVIOUS_RESULTS_DIR=$VIRGL_PATH/ci/previous_results/archived/${TEST_PATH}
+   fi
+
    IGNORE_TESTS_FILE=$PREVIOUS_RESULTS_DIR/ignore_tests.txt
 
    # Remove comments from test-list

commit 71e0626657e053acd389ac510be571f90fd41457

ci: Fix test results path for softpipe
In c20a5a9, I move some unused results files in the archived folder
but I didn't correctly fix the path of the softpipe results.
Softpipe is the driver used for the ci so its results file wasn't archived.

Fixes: c20a5a9 "ci: move outdated results to an archived folder"
Signed-off-by: Elie Tournier <elie.tournier@collabora.com>
Reviewed-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>

ci/run_test_suite.sh
@@ -298,7 +298,7 @@ create_result_dir()
    TEST_PATH=${HOST_GL}_host${HOST_DRIVER}/${TEST_APP}_${TEST_NAME}
    RESULTS_DIR=$VIRGL_PATH/results/${TEST_PATH}
 
-   if [ $HOST_DRIVER = softpipe ]; then
+   if [ "$HOST_DRIVER" = "_softpipe" ]; then
       PREVIOUS_RESULTS_DIR=$VIRGL_PATH/ci/previous_results/${TEST_PATH}
    else
       echo WARNING: Results are not up-to-date!