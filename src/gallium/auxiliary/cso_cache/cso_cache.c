2b0e2f0729b4c6b136e6023ff8026479000ed589
Gert Wollny <gert.wollny@collabora.com>
2021-01-19 12:13:11 +0100

    gallium/cso: use a different hash function
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
    Reviewed-by: Chia-I Wu <olvaffe@gmail.com>

--- src/gallium/auxiliary/cso_cache/cso_cache.c
+++ src/gallium/auxiliary/cso_cache/cso_cache.c
@@ -54,8 +54,10 @@ static unsigned hash_key(const void *key, unsigned key_size)
 
    /* I'm sure this can be improved on:
     */
-   for (i = 0; i < key_size/4; i++)
+   for (i = 0; i < key_size/4; i++) {
+      hash = (hash << 7) | (hash >> 25);
       hash ^= ikey[i];
+   }
 
    return hash;
 }
